f71c92f119d5c48d7ca4740b0758ebf3942bdaa2
Patelnk687 <patelnk687@gmail.com>
2023-03-21 20:41:55 -0500

    added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Card from "./Card";
-import CardList from "./CardList";
+
+import CardList from "./CardList"
 import "tachyons";
 import reportWebVitals from "./reportWebVitals";
 import { robots } from "./robots";
 
+
+
 ReactDOM.render(
-  <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-  </div>,
+  <div><CardList robots={robots}/>
+    </div>,
   document.getElementById("root")
 );
 
